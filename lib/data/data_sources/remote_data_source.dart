5ffb720a7b40f71358563166fb84ce1e253921a8
Wak Dyan <wakdyan@X555DG-0aee5ce0.localdomain>
2023-05-10 08:41:24 +0700

    add [getLeagues] method

--- lib/data/data_sources/remote_data_source.dart
+++ lib/data/data_sources/remote_data_source.dart
@@ -5,9 +5,13 @@ import 'package:kabar_olahraga/data/models/country_response.dart';
 
 import '../../common/exceptions.dart';
 import '../models/country_model.dart';
+import '../models/league_model.dart';
+import '../models/league_response.dart';
 
 abstract class RemoteDataSource {
   Future<List<CountryModel>> getCountries();
+
+  Future<List<LeagueModel>> getLeagues();
 }
 
 class ImplRemoteDataSource extends RemoteDataSource {
@@ -29,6 +33,27 @@ class ImplRemoteDataSource extends RemoteDataSource {
     final json = jsonDecode(result.body);
     if (result.statusCode == 200 || result.statusCode == 204) {
       return CountryResponse.fromJson(json).countries;
+    } else if (result.statusCode == 499) {
+      throw TimeOutException(json['message']);
+    } else if (result.statusCode == 500) {
+      throw ServerException(json['message']);
+    } else {
+      throw Exception('Unknown error');
+    }
+  }
+
+  @override
+  Future<List<LeagueModel>> getLeagues() async {
+    final result = await _client.get(
+      Uri.parse('${baseUrl}leagues'),
+      headers: {
+        'x-rapidapi-host': 'v3.football.api-sports.io',
+        'x-rapidapi-key': '5f2da4b3d7988f60b7acc56918d331c6',
+      },
+    );
+    final json = jsonDecode(result.body);
+    if(result.statusCode == 200||result.statusCode == 204){
+      return LeagueResponse.fromJson(json).leagues;
     } else if (result.statusCode == 499) {
       throw TimeOutException(json['message']);
     } else if (result.statusCode == 500) {
