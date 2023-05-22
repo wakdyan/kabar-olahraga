b3592ed75941bf85d0d50b455c9252c31842a9e8
Wak Dyan <aditya.dyan@yahoo.com>
2023-05-22 10:56:05 +0700

    override [getLeagues] function

--- lib/data/repositories/footbal_repository_impl.dart
+++ lib/data/repositories/footbal_repository_impl.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dartz/dartz.dart';
 import 'package:kabar_olahraga/data/data_sources/remote_data_source.dart';
 import 'package:kabar_olahraga/domain/entities/country.dart';
+import 'package:kabar_olahraga/domain/entities/league.dart';
 
 import '../../common/exceptions.dart';
 import '../../common/failure.dart';
@@ -19,6 +20,19 @@ class FootballRepositoryImpl extends FootballRepository {
       final result = await remoteDataSource.getCountries();
       final countries = result.map((e) => e.toEntity()).toList();
       return Right(countries);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(e.message));
+    } on SocketException {
+      return const Left(ConnectionFailure('Failed to connect to the network'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<League>>> getLeagues() async {
+    try {
+      final result = await remoteDataSource.getLeagues();
+      final leagues = result.map((e) => e.toEntity()).toList();
+      return Right(leagues);
     } on ServerException catch (e) {
       return Left(ServerFailure(e.message));
     } on SocketException {
