4f6d9485a49d8435057e734f097b64e46ab88105
Wak Dyan <aditya.dyan@yahoo.com>
2023-05-22 10:56:44 +0700

    add [getLeagues] function

--- lib/domain/repositories/footbal_repository.dart
+++ lib/domain/repositories/footbal_repository.dart
@@ -2,7 +2,9 @@ import 'package:dartz/dartz.dart';
 
 import '../../common/failure.dart';
 import '../entities/country.dart';
+import '../entities/league.dart';
 
 abstract class FootballRepository {
   Future<Either<Failure, List<Country>>> getCountries();
+  Future<Either<Failure, List<League>>> getLeagues();
 }
