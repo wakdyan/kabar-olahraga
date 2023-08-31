7b3014b5ce823da8aaa064fbe3919d90cb0ec04b
Wak Dyan <aditya.dyan@yahoo.com>
2023-08-31 10:43:01 +0700

    mocked [GetLeagues]

--- test/helpers/mock_class.dart
+++ test/helpers/mock_class.dart
@@ -1,10 +1,11 @@
 import 'package:kabar_olahraga/data/data_sources/remote_data_source.dart';
 import 'package:kabar_olahraga/domain/repositories/footbal_repository.dart';
+import 'package:kabar_olahraga/domain/usecase/get_leagues.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
 
 @GenerateMocks(
-  [RemoteDataSource, FootballRepository],
+  [RemoteDataSource, FootballRepository, GetLeagues],
   customMocks: [MockSpec<http.Client>(as: #MockHttpClient)],
 )
 void main() {}
