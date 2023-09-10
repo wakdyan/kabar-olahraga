1d8dedd6e618cc8d9bd25f96062f6ce2dc2b84d3
Wak Dyan <aditya.dyan@yahoo.com>
2023-09-10 11:10:22 +0700

    subtitute [mockito] with [mocktail] and change test description

--- test/domain/usecase/get_leagues.dart
+++ test/domain/usecase/get_leagues.dart
@@ -6,9 +6,9 @@ import 'package:kabar_olahraga/common/failure.dart';
 import 'package:kabar_olahraga/data/models/league_response.dart';
 import 'package:kabar_olahraga/domain/entities/league.dart';
 import 'package:kabar_olahraga/domain/usecase/get_leagues.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
-import '../../helpers/mock_class.mocks.dart';
+import '../../helpers/mock_class.dart';
 import '../../json_reader.dart';
 
 void main() {
@@ -16,17 +16,19 @@ void main() {
   final getLeagues = GetLeagues(mockRepository);
 
   test(
-    'should return list of leagues when request is successful',
+    'should return [data] when request is successful',
     () async {
       final matcher = <League>[];
-      final json = jsonDecode(readJson('dummy/countries_200.json'));
+      final json = jsonDecode(readJson('dummy/leagues_200.json'));
       final parsed = LeagueResponse.fromJson(json);
 
       for (var league in parsed.leagues) {
         matcher.add(league.toEntity());
       }
 
-      when(mockRepository.getLeagues()).thenAnswer((_) async => Right(matcher));
+      when(() => mockRepository.getLeagues()).thenAnswer((_) async {
+        return Right(matcher);
+      });
 
       final result = await getLeagues.execute();
       final resultList = result.getOrElse(() => []);
@@ -36,9 +38,9 @@ void main() {
   );
 
   test(
-    'should return failure when request is unsuccessful',
+    'should return [failure] when request is unsuccessful',
     () async {
-      when(mockRepository.getLeagues()).thenAnswer((_) async {
+      when(() => mockRepository.getLeagues()).thenAnswer((_) async {
         return const Left(ServerFailure(''));
       });
 
