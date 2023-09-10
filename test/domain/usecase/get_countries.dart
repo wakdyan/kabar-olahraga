dbb105c9b7d1f046acf480b7c0d2e3af9372196a
Wak Dyan <aditya.dyan@yahoo.com>
2023-09-10 11:05:18 +0700

    subtitute [mockito] with [mocktail] and change test description

--- test/domain/usecase/get_countries.dart
+++ test/domain/usecase/get_countries.dart
@@ -5,18 +5,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kabar_olahraga/common/failure.dart';
 import 'package:kabar_olahraga/data/models/country_response.dart';
 import 'package:kabar_olahraga/domain/entities/country.dart';
+import 'package:kabar_olahraga/domain/repositories/footbal_repository.dart';
 import 'package:kabar_olahraga/domain/usecase/get_countries.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
-import '../../helpers/mock_class.mocks.dart';
 import '../../json_reader.dart';
+
+class MockFootballRepository extends Mock implements FootballRepository {}
 
 void main() {
   final mockRepository = MockFootballRepository();
   final getCountries = GetCountries(mockRepository);
 
   test(
-    'should return list of countries when request is successful',
+    'should return [data] when request is successful',
     () async {
       final matcher = <Country>[];
       final json = jsonDecode(readJson('dummy/countries_200.json'));
@@ -26,7 +28,7 @@ void main() {
         matcher.add(country.toEntity());
       }
 
-      when(mockRepository.getCountries()).thenAnswer((_) async {
+      when(() => mockRepository.getCountries()).thenAnswer((_) async {
         return Right(matcher);
       });
 
@@ -38,9 +40,9 @@ void main() {
   );
 
   test(
-    'should return failure when request is unsuccessful',
+    'should return [failure] when request is unsuccessful',
     () async {
-      when(mockRepository.getCountries()).thenAnswer((_) async {
+      when(() => mockRepository.getCountries()).thenAnswer((_) async {
         return const Left(ServerFailure(''));
       });
 
