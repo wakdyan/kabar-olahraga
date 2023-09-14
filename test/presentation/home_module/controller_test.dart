a0aa8d0ebc1ab43b126caf05ffeffb6a1013ae0c
Wak Dyan <aditya.dyan@yahoo.com>
2023-09-14 09:50:33 +0700

    change test scenario

--- test/presentation/home_module/controller_test.dart
+++ test/presentation/home_module/controller_test.dart
@@ -1,58 +1,18 @@
-import 'dart:convert';
-
-import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
-import 'package:kabar_olahraga/common/enum.dart';
-import 'package:kabar_olahraga/common/failure.dart';
-import 'package:kabar_olahraga/data/models/league_response.dart';
-import 'package:kabar_olahraga/domain/entities/league.dart';
+import 'package:kabar_olahraga/common/constants.dart';
 import 'package:kabar_olahraga/presentation/home_module/controller.dart';
-import 'package:mocktail/mocktail.dart';
-
-import '../../helpers/mock_class.dart';
-import '../../json_reader.dart';
 
 void main() {
-  final usecase = MockGetLeagues();
-
   late HomeController controller;
 
   tearDown(() => Get.delete<HomeController>());
 
-  test('should update [leagues] when success', () async {
-    final matcher = <League>[];
-    final json = jsonDecode(readJson('dummy/leagues_200.json'));
-    final parsed = LeagueResponse.fromJson(json);
+  test('check are variables initialized', () {
+    controller = Get.put(HomeController());
 
-    for (var league in parsed.leagues) {
-      matcher.add(league.toEntity());
-    }
-
-    when(() => usecase.execute()).thenAnswer((_) async => Right(matcher));
-
-    controller = Get.put(HomeController(usecase));
-
-    await Future.delayed(const Duration(microseconds: 1));
-
-    expect(controller.leagues, matcher);
-    expect(controller.errorMessage, '');
-    expect(controller.requestState, RequestState.success);
-  });
-
-  test('should update [errorMessage] when failed', () async {
-    const matcher = 'Server failure';
-
-    when(() => usecase.execute()).thenAnswer((_) async {
-      return const Left(ServerFailure(matcher));
-    });
-
-    controller = Get.put(HomeController(usecase));
-
-    await Future.delayed(const Duration(microseconds: 1));
-
-    expect(controller.leagues, []);
-    expect(controller.errorMessage, matcher);
-    expect(controller.requestState, RequestState.error);
+    expect(controller.tabController.isBlank, false);
+    expect(controller.tabs.length, tabData.length);
+    expect(controller.pages.length, tabData.length);
   });
 }
