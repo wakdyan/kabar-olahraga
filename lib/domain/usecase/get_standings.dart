ae595c5dacfbeaedaec55551d1334b42ec8496e3
Wak Dyan <aditya.dyan@yahoo.com>
2023-09-14 10:00:27 +0700

    remove unused import

--- lib/domain/usecase/get_standings.dart
+++ lib/domain/usecase/get_standings.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:kabar_olahraga/domain/entities/league.dart';
 import 'package:kabar_olahraga/domain/repositories/footbal_repository.dart';
 
 import '../../common/failure.dart';
