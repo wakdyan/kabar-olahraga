63069a831ec219bd65fe8c44790ea2ef90370be8
Wak Dyan <aditya.dyan@yahoo.com>
2023-09-17 11:23:53 +0700

    add [toEntity] method

--- lib/data/models/standing_model.dart
+++ lib/data/models/standing_model.dart
@@ -1,5 +1,11 @@
 import 'package:equatable/equatable.dart';
-import '../../domain/entities/standing.dart' as m;
+
+import '../../domain/entities/league.dart' as e;
+import '../../domain/entities/match_history.dart' as e;
+import '../../domain/entities/standing.dart' as e;
+import '../../domain/entities/standing_detail.dart';
+import '../../domain/entities/team.dart' as e;
+
 part 'standing_model_league.dart';
 part 'standing_model_match.dart';
 part 'standing_model_standing.dart';
@@ -7,16 +13,45 @@ part 'standing_model_team.dart';
 
 class StandingModel extends Equatable {
   final League league;
-  final List<Standing> standings;
+  final List<List<Standing>> standings;
 
   const StandingModel({required this.league, required this.standings});
 
   factory StandingModel.fromJson(Map<String, dynamic> json) {
+    final league = json['league'];
     return StandingModel(
-      league: json['league'],
-      standings: List.from(json['standings'])
-          .map((e) => Standing.fromJson(e))
-          .toList(),
+      league: League.fromJson(league),
+      standings: List.from(league['standings']).map<List<Standing>>(
+        (e) {
+          return e.map<Standing>(
+            (e) {
+              return Standing.fromJson(e);
+            },
+          ).toList();
+        },
+      ).toList(),
+    );
+  }
+
+  e.Standing toEntity() {
+    return e.Standing(
+      league: league.toEntity(),
+      standings: standings[0].map(
+        (standings) {
+          final data = standings;
+          return StandingDetail(
+            rank: data.rank,
+            points: data.points,
+            goalsDiff: data.goalsDiff,
+            group: data.group,
+            form: data.form,
+            status: data.status,
+            description: data.description,
+            team: data.team.toEntity(),
+            matchHistory: data.all.toEntity(),
+          );
+        },
+      ).toList(),
     );
   }
 
