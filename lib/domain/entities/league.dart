58c07edcc05854a1800316d7c613f2431f3d2327
Wak Dyan <aditya.dyan@yahoo.com>
2023-09-18 11:23:27 +0700

    change field types

--- lib/domain/entities/league.dart
+++ lib/domain/entities/league.dart
@@ -3,11 +3,22 @@ import 'package:equatable/equatable.dart';
 class League extends Equatable {
   final int id;
   final String name;
-  final String type;
   final String logo;
+  final String country;
+  final String? type;
+  final String? season;
+  final String? flag;
 
-  const League(this.id, this.name, this.type, this.logo);
+  const League({
+    required this.id,
+    required this.name,
+    required this.logo,
+    required this.country,
+    this.type,
+    this.season,
+    this.flag,
+  });
 
   @override
-  List<Object?> get props => [id, name, type, logo];
+  List<Object?> get props => [id, name, type, logo, country, season, flag];
 }
