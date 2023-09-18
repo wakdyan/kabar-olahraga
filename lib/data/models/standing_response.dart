6c231cf2607ea463798effe528a71b2cd176c4c2
Wak Dyan <aditya.dyan@yahoo.com>
2023-09-18 11:12:49 +0700

    change type of [standings] variable

--- lib/data/models/standing_response.dart
+++ lib/data/models/standing_response.dart
@@ -2,14 +2,18 @@ import 'package:equatable/equatable.dart';
 import 'package:kabar_olahraga/data/models/standing_model.dart';
 
 class StandingResponse extends Equatable {
-  final StandingModel standing;
+  final List<StandingModel> standings;
 
-  const StandingResponse(this.standing);
+  const StandingResponse(this.standings);
 
   factory StandingResponse.fromJson(Map<String, dynamic> json) {
-    return StandingResponse(StandingModel.fromJson(json['response']));
+    return StandingResponse(
+      List.from(json['response']).map((e) {
+        return StandingModel.fromJson(e);
+      }).toList(),
+    );
   }
 
   @override
-  List<Object?> get props => [standing];
+  List<Object?> get props => [standings];
 }
