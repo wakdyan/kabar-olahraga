2fb0ece0b9572563e5447323a2283c7b4104c1cc
Wak Dyan <aditya.dyan@yahoo.com>
2023-09-19 12:04:20 +0700

    add dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart';
 
+import 'data/data_sources/remote_data_source.dart';
 import 'routes/app_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  Get.put(ImplRemoteDataSource(Client()));
 
   runApp(const MainApp());
 }
